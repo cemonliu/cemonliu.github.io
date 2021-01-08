6bd259afd478cc7722ef1b59e128dad005959504
cemonliu <cemon_liu@qq.com>
2021-01-08 12:44:59 +0800

    Site updated: 2021-01-08 12:44:58

--- themes/keep/source/js/main.js
+++ themes/keep/source/js/main.js
@@ -1,10 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   KEEP.themeInfo = {
+    theme: `Keep v${KEEP.theme_config.version}`,
     author: 'XPoet',
-    name: 'Keep',
-    version: KEEP.theme_config.version,
     repository: 'https://github.com/XPoet/hexo-theme-keep'
+  }
+
+  // print theme base info
+  KEEP.printThemeInfo = () => {
+    console.log(`\n %c ${KEEP.themeInfo.theme} %c ${KEEP.themeInfo.repository} \n`, `color: #fadfa3; background: #333; padding: 5px 0;`, `background: #fadfa3; padding: 5px 0;`);
   }
 
   KEEP.refresh = () => {
@@ -20,7 +24,12 @@ window.addEventListener('DOMContentLoaded', () => {
     if (KEEP.theme_config.code_copy.enable === true) {
       KEEP.initCodeCopy();
     }
+
+    if (KEEP.theme_config.lazyload.enable === true) {
+      KEEP.initLazyLoad();
+    }
   }
 
+  KEEP.printThemeInfo();
   KEEP.refresh();
 });
